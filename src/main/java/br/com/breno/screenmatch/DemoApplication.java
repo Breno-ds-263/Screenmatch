07d26f141badbb8bd03231ae9c77763d479495a5
Breno do nasicmento <Breno.ds263@gmail.com>
2025-01-07 22:51:54 -0300

    /home/eduardo/Documents/ARGUS/demo

--- src/main/java/br/com/breno/screenmatch/DemoApplication.java
+++ src/main/java/br/com/breno/screenmatch/DemoApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.breno.screenmatch.model.DadosSeries;
-import br.com.breno.screenmatch.services.ConsumoApi;
-import br.com.breno.screenmatch.services.ConverterDados;
+import br.com.breno.screenmatch.principal.Principal;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -17,16 +15,12 @@ public class DemoApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-	   var consumoapi = new ConsumoApi();
-	   var json = consumoapi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=b12890d5");
-	   
-	   System.out.println(json);
-	   
-	   ConverterDados conversor = new ConverterDados();
-	   
-	   DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
-	   System.out.println(dados);
+	  
+		Principal principal = new Principal();	
 		
+		principal.exibeMenu();
+	  
+
 		
 	}
 
